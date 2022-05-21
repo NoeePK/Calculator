29fa58246c70f04e726400f9ab64cb61cbd4271f
NoeePK <noeepisanokoffel@gmail.com>
2022-05-21 11:33:21 +0200

    ARGH

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+function Plus (number, number) {
+    let result = number + number;
+    return result;
+}
+
+function Minus (number, number) {
+    let result = number - number;
+    return result;
+}
+
+function Multiply (number, number) {
+    let result = number * number;
+    return result;
+}
+
+function Divide (number, number) {
+    let result = number / number;
+    return result;
+}
+
+function Equal () {
+    
+}
+
+function Result () {
+    result.style.display='block';
+    result.innerHTML = 'Résultat : ' + Equal();
+}
